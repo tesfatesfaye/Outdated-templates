3a14d44521be93fb57b6522b03219c597207e91d
Tesfa Tesfaye <108762211+tesfatesfaye@users.noreply.github.com>
2024-08-08 15:26:31 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS, TS, JSX, and TSX files in src directory
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
