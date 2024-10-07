a5f20bf0eae65c8ec939b4b6d64a244ebf08eddf
Tesfa Tesfaye <t@tesfaget.dev>
2024-10-07 17:04:50 -0400

    made many changes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
-
+import vercel from "vite-plugin-vercel";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), vercel()],
   server: {
-    open: true, // 
+    port: process.env.PORT,
+    open: true,
   },
 });
