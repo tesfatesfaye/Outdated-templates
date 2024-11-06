afadb5c6babf40126559760429f120a78e6e8975
Tesfa Tesfaye <t@tesfaget.dev>
2024-11-06 15:21:33 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import { Button } from "@/components/ui/button";
+
+const App=()=> {
+ 
+
+  return (
+    <>
+    <Button variant="outline" className=''>
+      button
+    </Button>
+    </>
+  )
+}
+
+export default App
